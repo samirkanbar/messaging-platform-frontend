7d12a458c7b4332e8c3fe3babb755fedb521fa90
Samir Kanbar <sam.kbr06@gmail.com>
2025-12-04 20:26:22 -0500

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,28 @@
+import { Button } from "@/registry/default/ui/button"
+
 export default function Home() {
   return(
     <div>
-      <h1>
-        SayGoodbyes.com
-      </h1>
+      <div>
+        <h1>
+          Say Goodbye, Even When You're Gone.
+        </h1>
+        <p>
+          Create a private list of people you want to send a final message to. 
+          Your designated dependent will ensure your goodbyes are delivered when the time comes.
+        </p>
+        <div>
+          <Button variant="outline">
+            Create an Account
+          </Button>
+          <Button>
+            Sign in
+          </Button>
+        </div>
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
